170acb8b61b721f667f4f40a5c3cf982528d63b8
EndarUliu <endar8@gmail.com>
2018-09-21 10:42:42 +0200

    update mongo

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -16,6 +16,41 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp');
 
     //update mongo object
+    // db.collection("Todos").findOneAndUpdate({
+    //     _id: new ObjectID("5ba262d17f66351de445bdc2")
+    // }, {
+    //     $set: {
+    //         complleted: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+
+    // }).then((resp) => {
+    //     console.log(resp);
+    // }, (e) => {
+    //     console.log(e)
+    // })
+
+
+    db.collection("Users").findOneAndUpdate({
+        _id: new ObjectID("5ba25ece7e09704205c2e3ed"),
+    }, {
+        $set: {
+            name: "Nacut"
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((resp) => {
+        console.log(resp);
+    }, (e) => {
+        console.log(e);
+    })
+
+
+
 
 
 
