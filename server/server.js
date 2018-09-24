83e7e33152fdfc7b5bd7088a3b2f35ee9fff88e8
EndarUliu <endar8@gmail.com>
2018-09-24 12:18:02 +0200

    get api added

--- server/server.js
+++ server/server.js
@@ -27,7 +27,11 @@ app.post('/todos', (req, res)=>{
 
 app.get('/todos', (req, res)=>{
    
-    
+       Todo.find().then((todos)=>{
+         res.status(200).send({todos});
+       },(err)=>{
+        res.status(400).send(err);
+       })
 });
 
 
