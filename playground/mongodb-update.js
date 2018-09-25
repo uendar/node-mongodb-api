f857e9d1dc1c0105bdfc491f4ea2c09e4c747703
EndarUliu <endar8@gmail.com>
2018-09-25 15:22:10 +0200

    update added to api

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -15,44 +15,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     console.log("Connected to MongoDB!");
     const db = client.db('TodoApp');
 
-    //update mongo object
-    // db.collection("Todos").findOneAndUpdate({
-    //     _id: new ObjectID("5ba262d17f66351de445bdc2")
-    // }, {
-    //     $set: {
-    //         complleted: true
-    //     }
-    // }, {
-    //     returnOriginal: false
-
-    // }).then((resp) => {
-    //     console.log(resp);
-    // }, (e) => {
-    //     console.log(e)
-    // })
-
-
-    db.collection("Users").findOneAndUpdate({
-        _id: new ObjectID("5ba25ece7e09704205c2e3ed"),
-    }, {
-        $set: {
-            name: "Nacut"
-        },
-        $inc: {
-            age: 1
-        }
-    }, {
-        returnOriginal: false
-    }).then((resp) => {
-        console.log(resp);
-    }, (e) => {
-        console.log(e);
-    })
-
-
-
-
-
-
+    
     // client.close();
 })
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID} = require('mongodb');
 
 var { mongoose} = require('./db/mongose');
@@ -27,7 +28,8 @@ app.post('/todos', (req, res)=>{
 app.get('/todos', (req, res)=>{
    
        Todo.find().then((todos)=>{
-         res.status(200).send({todos});
+           console.log(todos);
+         res.send(todos, 201)
        },(err)=>{
         res.status(400).send(err);
        });
@@ -76,9 +78,41 @@ app.delete('/todos/:id',(req, res)=>{
             }
             res.status(200).send({todo});
      }).catch((err)=>{
-        res.status(404).sstatus({message: 'id does not exists!'});
+        res.status(404).status({message: 'id does not exists!'});
      })
 
+});
+
+
+
+//patch used when update 
+app.patch('/todos/:id',(req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+   
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({message:'id does not exists!'})
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt  = new Date().getTime();  
+    }else{  
+        body.completed = false;
+        body.completedAt= null;
+    }
+
+
+    Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo)=>{
+
+        if(!todo){
+          return res.status(404).send({message:'No items with that id'})
+        }
+
+        res.status(200).send({todo})
+
+    }).catch((err)=>{
+           res.status(404).send({message: 'id does not exists!'});
+    });
 });
 
 
