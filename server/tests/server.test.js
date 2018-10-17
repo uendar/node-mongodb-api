aa965d8acf78b44c3814cbd5280bef0085f31906
EndarUliu <endar8@gmail.com>
2018-10-17 17:24:14 +0200

    describe for delete token added

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -342,6 +342,28 @@ describe('POST /users/login',()=>{
 
 });
 
+describe('DELETE /users/api/token',()=>{
+   it('should delete users token',(done)=>{
+        request(app)
+        .delete('/users/api/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)   
+        .end((err, res)=>{
+            if(err){
+                return done(err);
+            }
+
+            User.findById(users[0]._id).then((user)=>{
+              expect(user.tokens.length).toBe(0);
+              done();
+            }).catch((e)=>{
+                done(e);
+            })
+        });
+   });
+
+});
+
 
 
 
